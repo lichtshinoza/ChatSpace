7b42e44a66ac2fe7c61548824f413adac57a4f67
lichtshinoza <licht7xinoza@gmail.com>
2017-09-10 18:15:24 +0900

    create 'messages'controller, 'message'model, create DB and association :referenced by README.mb

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170909064847) do
+ActiveRecord::Schema.define(version: 20170910090234) do
 
   create_table "groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",       null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "messages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "msg"
+    t.string   "image"
+    t.integer  "group_id",   null: false
+    t.integer  "user_id",    null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
