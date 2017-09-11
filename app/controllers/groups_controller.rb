26e6f3e8af610c512eef493739e83686591ffd06
lichtshinoza <licht7xinoza@gmail.com>
2017-09-11 10:38:59 +0900

    add actions on group_controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,6 +3,21 @@ class GroupsController < ApplicationController
   def index
   end
 
-  def show
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+  def move_to_index
+      redirect_to action: :index unless user_signed_in?
+      # indexアクションを強制的に実行する
   end
 end
