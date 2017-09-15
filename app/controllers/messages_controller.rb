e2298924fad98c6d6c3fa8316ecf84d51eb3cef2
lichtshinoza <licht7xinoza@gmail.com>
2017-09-15 20:15:10 +0900

    form_for is modified, @group is setted

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,12 +1,14 @@
 class MessagesController < ApplicationController
 
   def index
+    @group = Group.find(params[:group_id])
     @groups = current_user.groups
     @message = Message.new
     @messages = Message.where(group_id: params[:group_id])
   end
 
   def create
+    @group = Group.find(params[:group_id])
     @message = Message.new(messages_params)
     if @message.save
       redirect_to group_messages_path, notice: "メッセージが投稿されました。"
