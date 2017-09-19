8c0738121ecfc728ae9fd897291a53487d0210a6
lichtshinoza <licht7xinoza@gmail.com>
2017-09-19 11:12:22 +0900

    message is generated by Faker

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :message do
-    msg "hello!"
+    msg Faker::Lorem.sentence
     image Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/image.png'))
     group
     user
