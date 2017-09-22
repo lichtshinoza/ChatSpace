1d610ba9d9990e0bb39dd8b77e63a210cb48f124
lichtshinoza <licht7xinoza@gmail.com>
2017-09-22 12:39:36 +0900

    simplyfy js

--- app/assets/javascripts/group.js
+++ app/assets/javascripts/group.js
@@ -1,24 +1,21 @@
 $(function(){
-  var search_list = $('.js-user-search-result');
-  function appendProduct(user) {
+  function appendProduct(user_id, user_name) {
     var html = `<div class='chat-group-user clearfix'>
-                  <p class='chat-group-user__name'>${user.name}</p>
-                  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add js-add-btn" data_user_id="${user.id}" data_user_name="${user.name}">追加</a>
+                  <p class='chat-group-user__name'>${user_name}</p>
+                  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add js-add-btn" data_user_id="${user_id}" data_user_name="${user_name}">追加</a>
                 </div>`
-    search_list.append(html);
+    return html;
   }
-
-  function appendNoproduct(user) {
+  function appendNoproduct(user_id, user_name) {
     var html = `<div class='chat-group-user clearfix'>
                   <p class='chat-group-user__name'>空欄が入力されました</p>
                   <a class="user-search-add chat-group-user__btn chat-group-user__btn--add js-add-btn" data_user_id="" data_user_name=""></a>
                 </div>`
-    search_list.append(html);
+    return html;
   }
 
   $("#user-search-field").on("keyup", function() {
     var input = $("#user-search-field").val();
-    console.log(input);
     $.ajax({
       type: 'GET',
       url: '/users/',
@@ -30,7 +27,10 @@ $(function(){
       $(".js-user-search-result").empty();
       if (users.length !== 0) {
         users.forEach(function(user){
-        appendProduct(user);
+        var user_id = user.id
+        var user_name = user.name
+        var html = appendProduct(user_id, user_name);
+        $('.js-user-search-result').append(html);
         });
       }
       else {
@@ -50,13 +50,7 @@ $(function(){
                   </div>`
     return html;
   }
-  function returnHTML(user_id, user_name){
-    var html = `<div class='chat-group-user clearfix'>
-                  <p class='chat-group-user__name'>${user_name}</p>
-                  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add js-add-btn" data_user_id="${user_id}" data_user_name="${user_name}">追加</a>
-                </div>`
-    return html;
-  }
+
   $(document).on('click','.js-add-btn', function(e){
     e.preventDefault();
     var user_id = $(this).attr('data_user_id');
@@ -69,7 +63,7 @@ $(function(){
     e.preventDefault();
     var user_id = $(this).attr('data_user_id');
     var user_name = $(this).attr('data_user_name');
-    var html = returnHTML(user_id, user_name);
+    var html = appendProduct(user_id, user_name);
     $('.js-user-search-result').append(html);
     $(this).parent().remove();
   });
